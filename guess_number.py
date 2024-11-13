f25b2b856195a002ea96f4ba441ebd1cdba6b48a
faiq296 <fadhlurrohim296@gmail.com>
2024-11-13 10:05:30 +0700

    commit pertama

--- guess_number.py
+++ guess_number.py
@@ -1,2 +1,14 @@
-# -*- coding: utf-8 -*-
-
+def guess_number():
+    secret_number = 9
+    guess = 0
+    guess_limit = 3
+    while guess < guess_limit:
+        user = int(input("masukkan angka >"))
+        if user == secret_number:
+            print("Selamat, anda berhasil menemukan angkanya")
+            break
+        else:
+            print("Salah!")
+            guess += 1
+    else:
+        print(f"Anda tidak menemukan angkanya, angka rahasianya adalah {secret_number}")
